a197ec58b6209040340fd06833ffe57d37ca25ad
Neal Lober <neal.lober@gmail.com>
2021-12-07 22:03:59 -0800

    Cleaned up the logo cloud scaling problem.

--- components/LogoCloud.vue
+++ components/LogoCloud.vue
@@ -5,35 +5,35 @@
         <p class="text-center font-semibold text-indigo-600 uppercase tracking-wide">
           Dozens of ERP integrations to automate transactions in your ERP system
         </p>
-        <div class="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+        <div class="mt-6 grid grid-cols-2 gap-6 md:grid-cols-4 lg:grid-cols-5">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/sfdc.svg" alt="Tuple">
           </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/sage200.svg" alt="Mirage">
           </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/sapbusinessone.svg" alt="StaticKit">
           </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/netsuitecrm.svg" alt="Transistor">
           </div>
-          <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/oraclesalescloud.svg" alt="Workcation">
           </div>
-          <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/quickbooks.svg" alt="Workcation">
           </div>
-          <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/twilio.svg" alt="Workcation">
           </div>
-          <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/msbusinesscentral.svg" alt="Workcation">
           </div>
-          <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/xerov2.svg" alt="Workcation">
           </div>
-          <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+          <div class="col-span-1 flex justify-center">
             <img class="h-32" src="~assets/logos/zuora.svg" alt="Workcation">
           </div>
         </div>
